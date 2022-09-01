44479e1c31ec7d2d360e871c02ec6b8bb6685b07
Driss <driss.b@ablaviation.com>
2022-09-01 11:10:18 +0100

    Add http request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'demoapp';
+export class AppComponent implements OnInit {
+
+  users: any = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get<any>('http://localhost:8000/api')
+      .subscribe(response => {
+        this.users = response.data
+        console.log(response);
+      })
+  }
 }
